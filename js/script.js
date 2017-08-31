bf776e832bab1462f734b7b741a84fc3930787b1
Your Name <buzinay@mail.ru>
2017-08-31 12:18:59 +0300

    Задание 5. Выполнила стилизацию проекта

---
+++ js/script.js
@@ -0,0 +1,47 @@
+var myMap;
+var myPlacemark;
+ymaps.ready(init);
+  function init () {
+    myMap = new ymaps.Map('map-id', {
+        center: [59.9394,30.33],
+        zoom: 16,
+        controls: ["zoomControl", "fullscreenControl"]
+  });
+
+myPlacemark = new ymaps.Placemark([59.938631, 30.323055],{
+  balloonContentBody: '',
+    },{
+        iconLayout: 'default#image',
+        iconImageHref: 'img/pin.svg',
+        iconImageSize: [80, 140],
+        iconImageOffset: [-40, -140]
+      });
+
+  myMap.geoObjects.add(myPlacemark);
+}
+
+var link = document.querySelector(".btn-contact");
+var popup = document.querySelector(".feedback");
+var overlay = document.querySelector(".feedback-overlay");
+var close = popup.querySelector(".modal-close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("feedback-show");
+  overlay.classList.add("feedback-overlay-show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("feedback-show");
+  overlay.classList.remove("feedback-overlay-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("feedback-show")) {
+      popup.classList.remove("feedback-show");
+      overlay.classList.remove("feedback-overlay-show");
+    }
+  }
+});
